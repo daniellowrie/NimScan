eb4e63534db1d3e12d7af35c1a0af3380fd4d37f
Daniel Lowrie <dlowrie290@yahoo.com>
2022-07-20 15:44:57 -0400

    Add files via upload

---
+++ nimscan.nim
@@ -0,0 +1,53 @@
+import scanner, os, strutils
+
+let numParams = paramCount()
+
+proc usage() =
+  echo """
+    ##################################################
+    # Usage:                                         #
+    # ./NimScan <targetIP> <start_PORT> <end_PORT>   #
+    #                                                #
+    #------------------------------------------------#
+    # Example:                                       #
+    # ./NimScan 192.168.1.140 21 1024                #
+    #                                                #
+    ##################################################"""
+
+proc main() =
+  if numParams > 0:
+    var targetIP:string = paramStr(1)
+    let startPort =  paramStr(2)
+    let endPort = paramStr(3)
+
+    let startPort_INT = parseInt(startPort)
+    let endPort_INT = parseInt(endPort)
+    
+    if startPort >= endPort:
+      echo "[!] Cannot have starting port number be higher than end port!"
+      echo ""
+      usage()
+    else:
+      echo ""
+      echo "Nim Port Scanner"
+      echo "================"
+      for PNUM in startPort_INT .. endPort_INT:
+        portScan(PNUM, targetIP)
+  else:
+    write(stdout, "Target IP: ")
+    let targetIP = readLine(stdin)
+    write(stdout, "START Port: ")
+    let startPort = readLine(stdin)
+    write(stdout, "END Port: ")
+    let endPort = readLine(stdin)
+
+    let startPort_INT = parseInt(startPort)
+    let endPort_INT = parseInt(endPort)
+    echo ""
+    echo "Nim Port Scanner"
+    echo "================"
+    for PNUM in startPort_int .. endPort_INT:
+      portScan(PNUM, targetIP)
+
+main()
+ 
---
+++ scanner.nim
@@ -0,0 +1,15 @@
+import net
+
+proc portScan*(PNUM: int, targetIP: string) =
+  var a = 0
+  var client = newSocket()
+  try:
+    client.connect(targetIP, Port(PNUM), 10)
+    a = 1
+  except OSError as e:
+    discard e
+  except TimeoutError as TE:
+    discard TE
+  finally:
+    if a == 1:
+      echo "Port Open: ", PNUM
