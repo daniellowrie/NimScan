882149921b0f983e74fe440ea89617aa3db1e944
Daniel Lowrie <dlowrie290@yahoo.com>
2022-07-20 16:51:14 -0400

    Update scanner.nim

--- scanner.nim
+++ scanner.nim
@@ -4,7 +4,7 @@ proc portScan*(PNUM: int, targetIP: string) =
   var a = 0
   var client = newSocket()
   try:
-    client.connect(targetIP, Port(PNUM), 10)
+    client.connect(targetIP, Port(PNUM), 110)
     a = 1
   except OSError as e:
     discard e
